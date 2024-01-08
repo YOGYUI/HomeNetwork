0e46615b6244b896e27425d4255769b31013d545
YOGYUI <lee2002w@gmail.com>
2024-01-08 11:20:37 +0900

    shell script typo fix

--- Hillstate-Gwanggyosan/activate.sh
+++ Hillstate-Gwanggyosan/activate.sh
@@ -1,6 +1,6 @@
 script_path=$(dirname $(realpath $0))
 PY_VENV_PATH=${script_path}/venv
-if [ ! -z "$PY_VENV_PATH" ]; then
+if [[ ! -d "$PY_VENV_PATH" ]]; then
     source ${script_path}/bootstrap.sh
 fi
 source ${PY_VENV_PATH}/bin/activate
